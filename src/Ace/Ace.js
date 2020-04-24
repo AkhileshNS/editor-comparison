b1c23b1c43653cc5f8bd0047730cd8b13afdefb7
AkhileshNS <nsakhilesh02@gmail.com>
2020-04-24 19:04:11 +0530

    Setup themes for both ace and monaro

--- src/Ace/Ace.js
+++ src/Ace/Ace.js
@@ -4,16 +4,35 @@ import AceEditor from 'react-ace';
 
 import './Ace.imports';
 import { themes, languages } from './Ace.constants';
+import { getModeForPath } from 'ace-builds/src-min-noconflict/ext-modelist';
 
 const Ace = () => {
   const [value, setValue] = React.useState('');
   const [themeValue, setTheme] = React.useState(themes[0]);
   const [lang, setLang] = React.useState(languages[0]);
 
+  const manageFiles = (e) => {
+    console.log(e.target.files[0]);
+    let reader = new FileReader();
+    reader.onload = (function (theFile) {
+      return function (e) {
+        setValue(e.target.result);
+      };
+    })(e.target.files[0]);
+    reader.readAsText(e.target.files[0]);
+
+    setLang(getModeForPath(e.target.files[0].name).name);
+  };
+
   return (
     <div className='ace'>
       <div>
         <Link to='/'>Back to Homepage</Link>
+        <input
+          style={{ marginLeft: '16px' }}
+          type='file'
+          onChange={manageFiles}
+        />
         <select
           style={{ marginLeft: '16px' }}
           value={themeValue}
--- src/Monaco/Monaco.js
+++ src/Monaco/Monaco.js
@@ -2,15 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { themes, languages, options } from './Monaco.constants';
 import Editor from 'react-monaco-editor';
+import { getModeForPath } from 'ace-builds/src-min-noconflict/ext-modelist';
 
 const Monaco = () => {
+  const [value, setValue] = React.useState('');
   const [language, setLanguage] = React.useState('javascript');
   const [theme, setTheme] = React.useState('vs-dark');
+
+  const addThemes = async (_, e) => {
+    try {
+      for (let theme in themes) {
+        if (theme !== 'vs-light' && theme !== 'vs-dark') {
+          let data = await import(
+            'monaco-themes/themes/' + themes[theme] + '.json'
+          );
+          e.editor.defineTheme(theme, data);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const manageFiles = (e) => {
+    console.log(e.target.files[0]);
+    let reader = new FileReader();
+    reader.onload = (function (theFile) {
+      return function (e) {
+        setValue(e.target.result);
+      };
+    })(e.target.files[0]);
+    reader.readAsText(e.target.files[0]);
+
+    setLanguage(getModeForPath(e.target.files[0].name).name);
+  };
 
   return (
     <div className='monaco'>
       <div style={{ marginBottom: '16px' }}>
         <Link to='/'>Back to Homepage</Link>
+        <input
+          style={{ marginLeft: '16px' }}
+          type='file'
+          onChange={manageFiles}
+        />
         <select
           style={{ marginLeft: '16px' }}
           value={theme}
@@ -31,9 +66,13 @@ const Monaco = () => {
       <Editor
         height='90vh' // By default, it fully fits with its parent
         theme={theme}
+        fontSize='24px'
         language={language}
         loading={'Loading...'}
         options={options}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+        editorDidMount={addThemes}
       />
     </div>
   );
